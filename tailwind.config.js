357df0beb04778ab66a53db5f2475613c1dd3242
AprildreamMI <magicwingzs@qq.com>
2022-09-07 16:50:45 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: 'tw-',
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/line-clamp')
+  ],
+}
