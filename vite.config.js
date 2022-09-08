7935a8f84defa41c6e6fb3bbf911c189d69206d5
zhuchen <zhuchen@yoyohr.com>
2022-09-08 14:24:29 +0800

    update: 更新项目结构

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { fileURLToPath, URL } from "node:url";
+import path from 'path'
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+
 import Components from "unplugin-vue-components/vite";
 import { VantResolver } from "unplugin-vue-components/resolvers";
 
@@ -16,6 +18,14 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+      '~/': `${path.resolve(__dirname, 'src')}/`
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "~/assets/styles/custom-var.scss" as *;`
+      }
     }
   }
 });
