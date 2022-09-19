14a5d0480902c54beedc180765f71883ceaa4f53
liqianchen <liqianchen@yoyohr.com>
2022-09-19 15:49:18 +0800

    修改配置文件

--- src/main.js
+++ src/main.js
@@ -7,8 +7,7 @@ import './assets/styles/index.scss'
 import 'vant/es/toast/style'
 import 'vant/es/dialog/style'
 const app = createApp(App)
-
+console.log('我是傻逼')
 app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -8,28 +8,34 @@ import Components from 'unplugin-vue-components/vite'
 import { VantResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/h5/',
-  server: {
-    host: '0.0.0.0'
-  },
-  plugins: [
-    vue(),
-    Components({
-      resolvers: [VantResolver()]
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '~/': `${path.resolve(__dirname, 'src')}/`
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "~/assets/styles/custom-var.scss" as *;`
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'production' ? '/qzsq/h5/' : '/',
+    server: {
+      host: '0.0.0.0'
+    },
+    plugins: [
+      vue(),
+      Components({
+        resolvers: [VantResolver()]
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '~/': `${path.resolve(__dirname, 'src')}/`
       }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "~/assets/styles/custom-var.scss" as *;`
+        }
+      }
+    },
+    esbuild: {
+      pure: ['console.log'],
+      minify: true
     }
   }
 })
