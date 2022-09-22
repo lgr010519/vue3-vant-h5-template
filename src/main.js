c29ff3542a7995616568a9dcaf10e23ef45f91c9
zhuchen <zhuchen@yoyohr.com>
2022-09-22 11:00:53 +0800

    update config

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './assets/styles/index.scss'
+
 import 'vant/es/toast/style'
 import 'vant/es/dialog/style'
+
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
