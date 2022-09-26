1ca4b860841ce3fdccebc088dec326ef978b402c
zhuchen <zhuchen@yoyohr.com>
2022-09-26 15:27:41 +0800

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 import path from 'path'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 import Components from 'unplugin-vue-components/vite'
@@ -9,8 +9,10 @@ import { VantResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
   return {
-    base: mode === 'production' ? '/qzsq/h5/' : '/',
+    base: env.VITE_BASE_URL,
     server: {
       host: '0.0.0.0'
     },
