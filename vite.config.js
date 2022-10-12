c68306afa0ecb4a595d77780fa0849bdfb4f03dc
liqianchen <liqianchen@yoyohr.com>
2022-10-12 16:04:59 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ import { VantResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
-
+  console.log(env.VITE_BASE_URL)
   return {
     base: env.VITE_BASE_URL,
     server: {
